4bea8504d730bd1baf6ea2ac65958a9ab1b82dc0
Josua052 <josuaronaldo96@gmail.com>
2023-01-13 23:54:32 +0700

    detailkomunitas/komber responsive 100%

--- src/Contents/MainKomber.jsx
+++ src/Contents/MainKomber.jsx
@@ -1,15 +1,15 @@
 import image1 from "../Assets/fotosd1.png";
 import image2 from "../Assets/logoinmengajar.png";
 import image3 from "../Assets/gambarmember.png";
-// import image6 from "../Assets/fotokomentar.png";
+import image6 from "../Assets/fotokomentar.png";
 import { DEKOM } from "../router";
 import { Link } from "react-router-dom";
 import { MdOutlineLinkedCamera } from "react-icons/md";
-// import { AiOutlineLike } from "react-icons/ai";
-// import { HiOutlineShare } from "react-icons/hi";
+import { AiOutlineLike } from "react-icons/ai";
+import { HiOutlineShare } from "react-icons/hi";
 import "../CSS/Komber.css";
 import Footer from "../Components/Footer";
-
+import "../CSS/Mobile/KomberMobile.css";
 
 const MainKomber = () => {
   
@@ -23,32 +23,36 @@ const MainKomber = () => {
         </Link>
       </div>
       <div className="posisikonten2">
-        <h1 className="textdeskripsi"> Deskripsi</h1>
-        <button type="button" className="buttongabung">
-          {" "}
-          Gabung
-        </button>
+        <div className="konten2top">
+          <h1 className="textdeskripsi"> Deskripsi</h1>
+          <button type="button" className="buttongabung">
+            {" "}
+            Gabung
+          </button>
+        </div>
         <p className="textkomunitasini">
           Komunitas ini berdiri dengan kekuatan alam yang sangat mendasar dari
           kekuatan uchiha dari negeri konoha yang mendalami peran menjadi...
         </p>
         <p className="textlihatselengkapnya"> Lihat Selengkapnya</p>
-        <img
-          style={{ marginLeft: "56px", position: "absolute" }}
-          src={image3}
-          alt="gambarmember"
-        />
-        <img
-          style={{ marginLeft: "96px", position: "absolute" }}
-          src={image3}
-          alt="gambarmember2"
-        />
-        <img
-          style={{ marginLeft: "136px", position: "absolute" }}
-          src={image3}
-          alt="gambarmember3"
-        />
-        <p className="text100member"> 100 member</p>
+        <div className="allmember">
+          <div className="fotomemberpilihan">
+            <img
+              
+              src={image3}
+              alt="gambarmember"
+            />
+            <img
+              src={image3}
+              alt="gambarmember2"
+            />
+            <img
+              src={image3}
+              alt="gambarmember3"
+            />
+          </div>
+          <p className="text100member"> 100 member</p>
+        </div>
       </div>
       <div className="posisikonten3">
         <textarea placeholder="Tulis pendapat kamu disini" />
@@ -57,47 +61,65 @@ const MainKomber = () => {
           Foto/Video
         </button>
       </div>
-      {/* <div className="posisikonten4">
-             <div className="status">
-          <img className="imgkomentar" src={image6} alt="fotokomentar" />
-          <h1 className="textnamamember"> Ngab Dim</h1>
-          <p className="textkomentar">
-            Barang siapa yang suka membantu surga adalah tempatnya
-          </p>
-          <p className="like">
-            <AiOutlineLike size={30} /> Ga ada yang suka
-          </p>
-          <p className="share">
-            <HiOutlineShare size={30} /> Bagikan
-          </p>
+      <div className="posisikonten4">
+          <div className="status">
+            <div className="detailstatus">
+              <div className="detailstatustop">
+                <img className="imgkomentar" src={image6} alt="fotokomentar" />
+                <h1 className="textnamamember"> Ngab Dim</h1>
+              </div>
+              <p className="textkomentar">
+              Barang siapa yang suka membantu surga adalah tempatnya
+              </p>
+            </div>
+            <div className="detailstatusbottom">
+              <p className="like">
+              <AiOutlineLike size={30} /> Ga ada yang suka
+              </p>
+              <p className="share">
+                <HiOutlineShare size={30} /> Bagikan
+              </p>
+            </div>
         </div>
         <div className="status">
-          <img className="imgkomentar" src={image6} alt="fotokomentar" />
-          <h1 className="textnamamember"> Ngab Dim</h1>
-          <p className="textkomentar">
-            Barang siapa yang suka membantu surga adalah tempatnya
-          </p>
-          <p className="like">
-            <AiOutlineLike size={30} /> Ga ada yang suka
-          </p>
-          <p className="share">
-            <HiOutlineShare size={30} /> Bagikan
-          </p>
+            <div className="detailstatus">
+              <div className="detailstatustop">
+                <img className="imgkomentar" src={image6} alt="fotokomentar" />
+                <h1 className="textnamamember"> Ngab Dim</h1>
+              </div>
+              <p className="textkomentar">
+              Barang siapa yang suka membantu surga adalah tempatnya
+              </p>
+            </div>
+            <div className="detailstatusbottom">
+              <p className="like">
+              <AiOutlineLike size={30} /> Ga ada yang suka
+              </p>
+              <p className="share">
+                <HiOutlineShare size={30} /> Bagikan
+              </p>
+            </div>
         </div>
         <div className="status">
-          <img className="imgkomentar" src={image6} alt="fotokomentar" />
-          <h1 className="textnamamember"> Ngab Dim</h1>
-          <p className="textkomentar">
-            Barang siapa yang suka membantu surga adalah tempatnya
-          </p>
-          <p className="like">
-            <AiOutlineLike size={30} /> Ga ada yang suka
-          </p>
-          <p className="share">
-            <HiOutlineShare size={30} /> Bagikan
-          </p>
+            <div className="detailstatus">
+              <div className="detailstatustop">
+                <img className="imgkomentar" src={image6} alt="fotokomentar" />
+                <h1 className="textnamamember"> Ngab Dim</h1>
+              </div>
+              <p className="textkomentar">
+              Barang siapa yang suka membantu surga adalah tempatnya
+              </p>
+            </div>
+            <div className="detailstatusbottom">
+              <p className="like">
+              <AiOutlineLike size={30} /> Ga ada yang suka
+              </p>
+              <p className="share">
+                <HiOutlineShare size={30} /> Bagikan
+              </p>
+            </div>
         </div>
-      </div> */}
+      </div>
       <Footer />
     </>
   );
