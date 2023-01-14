51838aa99339b1fc4e8d3894f623ed2dccfc622d
Bill Delvin <billdelvin6@gmail.com>
2023-01-14 18:39:27 +0700

    adding auth controller

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,16 +1,15 @@
-import { Navbar } from "react-bootstrap";
-import logo from "../Assets/logo.png";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Dropdown } from "react-bootstrap";
+import jwtDecode from "jwt-decode";
+import logo from "../Assets/logo.png";
 // import Headroom from "react-headroom";
-import {
-  MASUK,
-  KOMUNITAS,
-  HOME,
-  AKSI,
-  DAFTAR,
-  ABOUTUS,
-  ARTIKEL,
-} from "../router";
+import { MASUK, KOMUNITAS, HOME, AKSI, DAFTAR, ABOUTUS, ARTIKEL } from "../router";
+import { CgProfile } from "react-icons/cg";
+import { HiUserGroup } from "react-icons/hi";
+import { MdOutlineNotificationsActive } from "react-icons/md";
+import { MdExitToApp } from "react-icons/md";
+
 import "../CSS/Navigation.css";
 import React from "react";
 import Burger from "./HamburgerMenu";
@@ -18,53 +17,136 @@ import Burger from "./HamburgerMenu";
 // import Burger from "./HamburgerMenu";
 // import MobileNav from "./MobileView";
 
+const usernameLength = (name) => {
+  let convertName = name;
+
+  if (convertName.length > 5) {
+    convertName = convertName.substr(0, 5) + "...";
+  }
+
+  return convertName;
+};
+
 const Navigation = () => {
+  const navRef = useRef({
+    isLogin: false,
+    username: "",
+  });
+
+  const validateUser = () => {
+    const tokenExists = localStorage.getItem(process.env.REACT_APP_AUTH);
+    if (tokenExists) {
+      const token = tokenExists.split(" ")[1];
+      const { username } = jwtDecode(token);
+      const isExistsToken = token !== undefined ? true : false;
+      navRef.current = {
+        isLogin: isExistsToken,
+        username,
+      };
+    }
+  };
+
+  validateUser();
   // const [open, setOpen] = useState(false);
+
+  const displayButton = !navRef.current.isLogin ? (
+    <div className="buttonloginlogout">
+      <Link style={{ textDecoration: "none" }} to={MASUK}>
+        <button type="button" className="button-masuk">
+          Masuk
+        </button>
+      </Link>
+      <Link style={{ textDecoration: "none" }} to={DAFTAR}>
+        <button type="button" className="button-daftar">
+          Daftar
+        </button>
+      </Link>
+    </div>
+  ) : (
+    <div className="mainmenuright">
+      <MdOutlineNotificationsActive className="position-icon-notif" size={30} />
+      <Dropdown>
+        <Dropdown.Toggle
+          className="dropdownnavprofil"
+          style={{
+            width: "138px",
+            height: "56px",
+            fontSize: "18px",
+            fontWeight: "500",
+            marginTop: "6px",
+          }}
+          variant="transparant"
+          id="dropdown-basic"
+        >
+          <CgProfile className="icon-profil" size={30} />
+          {usernameLength(navRef.current.username)}
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Link
+            className="dropdown-item"
+            style={{
+              display: "flex",
+              gap: "10px",
+              textDecoration: "none",
+              color: "black",
+              fontSize: "18px",
+            }}
+            to="/Profile"
+          >
+            <CgProfile size={30} />
+            Profile
+          </Link>
+
+          <Dropdown.Item style={{ display: "flex", gap: "10px", fontSize: "18px" }}>
+            <HiUserGroup size={30} />
+            Komunitas
+          </Dropdown.Item>
+          <Link
+            className="dropdown-item"
+            style={{
+              display: "flex",
+              gap: "10px",
+              textDecoration: "none",
+              color: "black",
+              fontSize: "18px",
+            }}
+            to={HOME}
+          >
+            <MdExitToApp size={30} />
+            Keluar
+          </Link>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
+
   return (
     <>
       {/* <Headroom> */}
       <Navbar className="navbar" bg="transparant">
-        <Navbar.Brand href="#home">
+        <Navbar.Brand>
           <img className="logo" src={logo} alt="logo" />
         </Navbar.Brand>
         <div className="menuheader">
           <div className="mainmenu">
             <Link style={{ color: "black" }} className="linknav" to={HOME}>
-              <li className="styling-menu-navbar animasi-left-right">
-                Beranda
-              </li>
+              <li className="styling-menu-navbar animasi-left-right">Beranda</li>
             </Link>
             <Link style={{ color: "black" }} className="linknav" to={KOMUNITAS}>
-              <li className="styling-menu-navbar animasi-left-right">
-                Komunitas
-              </li>
+              <li className="styling-menu-navbar animasi-left-right">Komunitas</li>
             </Link>
             <Link style={{ color: "black" }} className="linknav" to={ARTIKEL}>
-              <li className="styling-menu-navbar animasi-left-right">
-                Artikel
-              </li>
+              <li className="styling-menu-navbar animasi-left-right">Artikel</li>
             </Link>
             <Link style={{ color: "black" }} className="linknav" to={AKSI}>
               <li className="styling-menu-navbar animasi-left-right">Aksi</li>
             </Link>
             <Link style={{ color: "black" }} to={ABOUTUS}>
-              <li className="styling-menu-navbar animasi-left-right">
-                Tentang Kami
-              </li>
+              <li className="styling-menu-navbar animasi-left-right">Tentang Kami</li>
             </Link>
           </div>
-          <div className="buttonloginlogout">
-            <Link style={{ textDecoration: "none" }} to={MASUK}>
-              <button type="button" className="button-masuk">
-                Masuk
-              </button>
-            </Link>
-            <Link style={{ textDecoration: "none" }} to={DAFTAR}>
-              <button type="button" className="button-daftar">
-                Daftar
-              </button>
-            </Link>
-          </div>
+          {displayButton}
         </div>
         <Burger />
       </Navbar>
